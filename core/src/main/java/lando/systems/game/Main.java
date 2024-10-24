d30c2801fdddc38086aa3c5f71218cb9392fa2bb
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2024-10-23 19:23:52 -0500

    Add the ui-skin from rockbite/talos

--- core/src/main/java/lando/systems/game/Main.java
+++ core/src/main/java/lando/systems/game/Main.java
@@ -2,33 +2,67 @@ package lando.systems.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.kotcrab.vis.ui.VisUI;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    OrthographicCamera camera;
+    SpriteBatch batch;
+    Texture gdx;
+    TextureAtlas atlas;
+    Skin skin;
+    Stage stage;
 
     @Override
     public void create() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        gdx = new Texture("libgdx.png");
+        atlas = new TextureAtlas(Gdx.files.internal("ui/skin-talos/uiskin.atlas"));
+        skin = new Skin(Gdx.files.internal("ui/skin-talos/uiskin.json"), atlas);
+        skin.addRegions(atlas);
+        stage = new Stage();
+        VisUI.load(skin);
+    }
+
+    public void update(float dt) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            Gdx.app.exit();
+        }
+
+        stage.act(dt);
+        camera.update();
     }
 
     @Override
     public void render() {
+        float dt = Gdx.graphics.getDeltaTime();
+        update(dt);
+
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(gdx, 140, 210);
         batch.end();
+
+        stage.draw();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        gdx.dispose();
+        atlas.dispose();
+        VisUI.dispose();
     }
 }
